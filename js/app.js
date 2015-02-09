df87459437f29766b9151a09b15870b011b177ae
Kyle Paul <trinominis@Kyles-MacBook-Pro.local>
2015-02-09 02:28:13 -0600

    Step 8: Implementing a User service

--- js/app.js
+++ js/app.js
@@ -52,7 +52,32 @@ angular.module('myApp', ['ngRoute'])
     .otherwise({redirectTo: '/'});
 })
 
-.controller('MainCtrl', function($scope, $timeout, Weather) {
+.factory('UserService', function() {
+  var defaults = {
+    location: 'autoip'
+  };
+
+  var service = {
+    user: {},
+    save: function() {
+      sessionStorage.presently =
+        angular.toJson(service.user);
+    },
+    restore: function() {
+      // Pull from sessionStorage
+      service.user =
+        angular.fromJson(sessionStorage.presently) || defaults
+
+      return service.user;
+    }
+  };
+  // Immediately call restore from the session storage
+  // so we have our user data available immediately
+  service.restore();
+  return service;
+})
+
+.controller('MainCtrl', function($scope, $timeout, Weather, UserService) {
   // Build the date object
   $scope.date = {};
 
@@ -63,17 +88,21 @@ angular.module('myApp', ['ngRoute'])
   }
 
   $scope.weather = {}
-  // Hardcode San_Francisco for now
-  Weather.getWeatherForecast("CA/San_Francisco")
+  $scope.user = UserService.user;
+  Weather.getWeatherForecast($scope.user.location)
     .then(function(data) {
       $scope.weather.forecast = data;
-    });
+  });
 
   // Kick off the update function
   updateTime();
 })
 
 .controller('SettingsCtrl',
-  function($scope) {
-    // Our controller will go here
+  function($scope, UserService) {
+    $scope.user = UserService.user;
+
+    $scope.save = function() {
+      UserService.save();
+    }
 });
