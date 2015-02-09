ad4c39eee8ecd7a1648d7d524297363d43b651b0
Kyle Paul <trinominis@Kyles-MacBook-Pro.local>
2015-02-09 02:12:02 -0600

    Step 6: Building the angular service

--- js/app.js
+++ js/app.js
@@ -1,5 +1,45 @@
 angular.module('myApp', [])
-.controller('MainCtrl', function($scope, $timeout) {
+.provider('Weather', function() {
+  var apiKey = "";
+
+  this.setApiKey = function(key) {
+    if (key) this.apiKey = key;
+  };
+
+  this.getUrl = function(type, ext) {
+    return "http://api.wunderground.com/api/" +
+      this.apiKey + "/" + type + "/q/" +
+      ext + '.json';
+  };
+
+  this.$get = function($q, $http) {
+    var self = this;
+    return {
+      getWeatherForecast: function(city) {
+        var d = $q.defer();
+        $http({
+          method: 'GET',
+          url: self.getUrl("forecast", city),
+          cache: true
+        }).success(function(data) {
+          // The wunderground API returns the
+          // object that nests the forecasts inside
+          // the forecast.simpleforecast key
+          d.resolve(data.forecast.simpleforecast);
+        }).error(function(err) {
+          d.reject(err);
+        });
+        return d.promise;
+      }
+    }
+  }
+})
+
+.config(function(WeatherProvider) {
+  WeatherProvider.setApiKey('227410e53fceeda9');
+})
+
+.controller('MainCtrl', function($scope, $timeout, Weather) {
   // Build the date object
   $scope.date = {};
 
@@ -8,6 +48,13 @@ angular.module('myApp', [])
     $scope.date.raw = new Date();
     $timeout(updateTime, 1000);
   }
+
+  $scope.weather = {}
+  // Hardcode San_Francisco for now
+  Weather.getWeatherForecast("CA/San_Francisco")
+    .then(function(data) {
+      $scope.weather.forecast = data;
+    });
 
   // Kick off the update function
   updateTime();
